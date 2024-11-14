5b2c863d07cf4f3b989b1f5deb57a03181c1dee5
chimdi247 <Chimdi247@gmail.com>
2024-11-14 17:04:41 +0200

    updataed cron.js file

--- api/cron/cron.js
+++ api/cron/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "/";
+const URL = "https://real-estate-raph.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
